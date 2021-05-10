4cca45b66edadf6bd8ee18cd035a3f78da00f149
hanxuejie <1840873908@qq.com>
2021-05-10 08:33:27 +0800

    添加了map工具转换

--- src/edu/xcdq/tools/Demo03.java
+++ src/edu/xcdq/tools/Demo03.java
@@ -1,6 +1,6 @@
 package edu.xcdq.tools;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * @Author:hanxuejie
@@ -13,6 +13,17 @@ public class Demo03 {
         map.put("1","A");
         map.put("2","B");
         map.put("3","C");
-
+        //1
+        Set<String>keys=map.keySet();
+        System.out.println(keys);
+        //2把set转换成其他集合
+        ArrayList<String>alistkeys=new ArrayList<>(keys);
+        System.out.println(alistkeys);
+        //3
+        Collection<String> v=map.values();
+        System.out.println(v);
+        //4
+        LinkedList<String>llistv=new LinkedList<>(v);
+        System.out.println(llistv);
     }
 }
